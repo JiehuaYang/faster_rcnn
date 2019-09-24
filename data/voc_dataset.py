82327bf5a4661f94b50c841a58eae39fdee0ead1
jhyang <jiehuayang@hust.edu.cn>
2019-09-24 22:27:11 +0800

    update

--- data/voc_dataset.py
+++ data/voc_dataset.py
@@ -1,6 +1,8 @@
 import numpy as np
-
+import os
 #处理特定数据集对应的标注
 # 首先是路径，其次是用的哪一部分的数据集train val 还是test 是否使用difficult这种标注
 class VOCDataset:
-    def __init__(self, data_dir, split='trainval', use_difficult = False, return_difficult = False)
+    def __init__(self, data_dir, split='trainval', use_difficult = False, return_difficult = False):
+        id_list = os.path.join(data_dir,'ImageSets/Main/{0}.txt'.format(split))
+    
