5f77a3924f743ec609c93eec7d9baa53f7c275bd
jhyang <jiehuayang@hust.edu.cn>
2019-09-25 11:47:58 +0800

    update

--- data/voc_dataset.py
+++ data/voc_dataset.py
@@ -14,4 +14,26 @@ class VOCDataset:
 
         self.label_names = VOC_BBOX_LABEL_NAMES
 
-        
+
+
+VOC_BBOX_LABEL_NAMES = (
+    'aeroplane',
+    'bicycle',
+    'bird',
+    'boat',
+    'bottle',
+    'bus',
+    'car',
+    'cat',
+    'chair',
+    'cow',
+    'diningtable',
+    'dog',
+    'horse',
+    'motorbike',
+    'person',
+    'pottedplant',
+    'sheep',
+    'sofa',
+    'train',
+    'tvmonitor')
