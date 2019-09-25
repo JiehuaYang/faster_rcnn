8fc1e2fd4dd5c0fe68870416355925ed9ef4422b
jhyang <jiehuayang@hust.edu.cn>
2019-09-25 11:32:57 +0800

    update

--- data/voc_dataset.py
+++ data/voc_dataset.py
@@ -4,5 +4,6 @@ import os
 # 首先是路径，其次是用的哪一部分的数据集train val 还是test 是否使用difficult这种标注
 class VOCDataset:
     def __init__(self, data_dir, split='trainval', use_difficult = False, return_difficult = False):
+        #先获取id列表
         id_list = os.path.join(data_dir,'ImageSets/Main/{0}.txt'.format(split))
         self.ids = [id.strip() for id in open(id_list)]
